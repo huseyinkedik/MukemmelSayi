e0842bf12d44b7039090ce41abcdd64dd195922a
Huseyin <159181717+huseyinkedik@users.noreply.github.com>
2024-02-21 10:58:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,14 @@ public class Main {
         System.out.print("Sayı giriniz: ");
         int n = input.nextInt();
 
-        //Mükemmel sayı koşulu
+        //Mükemmel sayı döngüsü
         int total = 0;
         for (int i = 1; i < n; i++) {
             if (n % i == 0) {
                 total += i;
             }
         }
+        ///Mükemmel sayı koşulu
         if (total == n) {
             System.out.println(n + " Mükemmel sayıdır");
         } else {
